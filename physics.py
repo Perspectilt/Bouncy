e5e92c2cb81283a4d2757287e7fd4a178491c5c8
ghost1608 <64543976+ghost-1608@users.noreply.github.com>
2020-07-30 19:38:14 +0530

    Update physics.py
    Updated to pygame.

--- physics.py
+++ physics.py
@@ -3,6 +3,7 @@
 import math
 import os.path
 import time
+import pygame
 
 
 # Adds force (vector) to an object
@@ -15,7 +16,7 @@ def addforce(object, name, magnitude, direction):
             c = False
             break
 
-    if c:
+    if c and magnitude:
         object.forces += [{'name': name.lower(), 'magnitude': math.fabs(float(magnitude)), 'direction': float(direction)}]
 
 
@@ -34,11 +35,11 @@ def apply_gravity(objects):
 
 # Main physics handle:-
 
-T, t, xvector, yvector, u_x, u_y, n, s0x, s0y = 0, 0, 0, 0, 0, 0, 1, 0, 0
+T, t, xvector, yvector, u_x, u_y, n, s0x, s0y, condition, s_x, s_y = 0, 0, 0, 0, 0, 0, 1, 0, 0, True, 0, 0
 
 
 # To add the laws of physics (very basic stuff for now -- limited to 1 object and 1 boundary only)
-def physics(root, object, boundary, log=False):
+def physics(object, boundary, time_factor=1, log=False, limit=False):
     global t
     global xvector
     global yvector
@@ -48,6 +49,13 @@ def physics(root, object, boundary, log=False):
     global s0x
     global s0y
     global T
+    global condition
+    global s_x
+    global s_y
+    
+    p_factor = 1      # Conversion factor for metre per pixel
+
+    time_factor = (int(time_factor), 1)[not int(time_factor)]
 
     # Records original position for reference
     if t == 0:
@@ -80,8 +88,11 @@ def physics(root, object, boundary, log=False):
             f.write('\tyvector: ' + str(yvector) + '\n')
 
     # Calculations for x, y positions and velocities at current time 't' (rounded, as coordinates are pixels)
-    s_x, s_y = s0x - round(u_x * t + .5 * xvector/object[0].mass * t ** 2), s0y - round(u_y * t + .5 * yvector/object[0].mass * t ** 2)
     v_x, v_y = round(u_x + xvector/object[0].mass * t), round(u_y + yvector/object[0].mass * t)
+    if condition:
+        s_x, s_y = s0x - round((v_x - u_x) * t), s0y - round((v_y - u_y))
+    else:
+        condition = True
 
     # Logging
     if log:
@@ -90,9 +101,8 @@ def physics(root, object, boundary, log=False):
             f.write('\tv_x: ' + str(v_x) + '\tv_y: ' + str(v_y) + '\n')
 
     # Gets rid of instantaneous forces
-    if not (object[0].pos_x <= boundary[0].pos()[0] or object[0].pos()[2] >= boundary[0].pos()[2]):
+    if t == 1 + .01 * time_factor or t == 1 + .02 * time_factor:
         removeforce(object[0], 'f_x')
-    if not (object[0].pos()[1] <= boundary[0].pos()[1] or object[0].pos()[3] >= boundary[0].pos()[3]):
         removeforce(object[0], 'f_y')
 
     # Logging
@@ -107,44 +117,46 @@ def physics(root, object, boundary, log=False):
     # Logging
     if log:
         with open('debug.log', 'a') as f:
-            f.write('\tobject[0]_at: ' + '(' + str(object[0].pos()[0]) + ',' + str(object[0].pos()[1]) + ')\n')
+            f.write('\tobject[0].pos()[0:2]: ' + str(object[0].pos()[0:2]) + '\n')
 
     # Changes v to u for the next iteration
     u_x, u_y = v_x, v_y
 
     # Collision physics (assumes completely elastic collision)  <!!!Fine-tuning needed!!!>
-    if (v_x != 0) and (object[0].pos()[0] <= boundary[0].pos()[0] or object[0].pos()[2] >= boundary[0].pos()[2]):
-        addforce(object[0], 'f_x', 2 * object[0].mass * u_x/(round(t), 1)[round(t) == 0], -math.copysign(1, object[0].mass * u_x/(round(t), 1)[round(t) == 0]) * 90)
+    if (v_x > 0 and object[0].pos()[0] <= boundary[0].pos()[0]) or (v_x < 0 and object[0].pos()[2] >= boundary[0].pos()[2]):
+        addforce(object[0], 'f_x', 2 * object[0].mass * u_x * p_factor/(round(t), 1)[round(t) == 0] + xvector, -math.copysign(1, object[0].mass * u_x/(round(t), 1)[round(t) == 0]) * 90)
         t = 1
         s0x, s0y = object[0].pos()[0], object[0].pos()[1]
         v_x, v_y = 0, 0
-    if (v_y != 0) and (object[0].pos()[1] <= boundary[0].pos()[1] or object[0].pos()[3] >= boundary[0].pos()[3]):
-        addforce(object[0], 'f_y', 2 * object[0].mass * u_y/(round(t), 1)[round(t) == 0], -math.copysign(1, object[0].mass * u_y/(round(t), 1)[round(t) == 0]) * 90)
+        condition = False       # condition for skipping position calculation while impulsive force exists
+    if (v_y > 0 and (object[0].pos()[1] <= boundary[0].pos()[1])) or (v_y < 0 and (object[0].pos()[3] >= boundary[0].pos()[3])):
+        addforce(object[0], 'f_y', 2 * object[0].mass * u_y * p_factor/(round(t), 1)[round(t) == 0] + yvector, -math.copysign(1, object[0].mass * u_y/(round(t), 1)[round(t) == 0]) * 90)
         t = 1
         s0x, s0y = object[0].pos()[0], object[0].pos()[1]
         v_x, v_y = 0, 0
+        condition = False       # condition for skipping position calculation while impulsive force exists
 
-
-    t += .01   # Time counter (to be replaced in next version?)
-    T += .01   # Master time-keeper
+    t += .01 * time_factor   # Time counter (to be replaced in next version?)
+    T += .01 * time_factor   # Master time-keeper
 
     # Logging
     if log:
         with open('debug.log', 'a') as f:
             f.write('\tobject[0].forces: ' + str(object[0].forces) + '\n')
 
-
-
     # Logging
     if log:
         with open('debug.log', 'a') as f:
             f.write('\tu_x: ' + str(u_x) + '\tu_y: ' + str(u_y) + '\n')
             f.write('\tv_x: ' + str(v_x) + '\tv_y: ' + str(v_y) + '\n')
-            f.write('\tT: ' + str(round(T)) + '\n')
+            f.write('\tt: ' + str(t) + '\n')
+            f.write('\tround(T): ' + str(round(T)) + '\n')
             f.write('\n\n')
         n += 1
 
     # Limiting the loop at 1 iteration per 10ms
-    time.sleep(.01)
+    time.sleep(.01 * time_factor)
 
-    # root.after(1, physics)        <--- Doesnt work yet :(
+    if limit:
+        if round(T) == limit:
+            pygame.quit()
