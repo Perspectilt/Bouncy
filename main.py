aaf1b1a840a02c6e7fdae115e6a4d24a001f4dae
ghost1608 <64543976+ghost-1608@users.noreply.github.com>
2020-07-30 19:34:18 +0530

    Update main.py
    Updated to pygame version.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import tkinter
+import pygame
 import res
 import physics
 
 # Basic stuff
-root = tkinter.Tk()
-root.geometry('1024x768')
-canvas = tkinter.Canvas(root, width=1024, height=768)
-root.title('Bouncy')
-
-canvas.pack()
+pygame.init()
+screen = pygame.display.set_mode((1024, 768))
+screen.fill((255, 255, 255))
+pygame.display.set_caption('Bouncy')
 
 # Create a ball and a container
-ball_1 = res.Ball(canvas, pos_x=100, pos_y=665, radius=50, mass=1)
-container_1 = res.Container(canvas, pos_x=0, pos_y=0, width=1024, height=768)
+ball_1 = res.Ball(screen, pos_x=500, pos_y=100, radius=50, mass=1)
+container_1 = res.Container(screen, pos_x=0, pos_y=0, width=1024, height=768)
 
 # Add the object and the boundaries to their respective global lists
 objects = [ball_1]
@@ -21,8 +19,17 @@ boundaries = [container_1]
 # Applying gravity to all objects
 physics.apply_gravity(objects)
 
-for n in range(1000):         # <-- Temp measure
-    physics.physics(root, objects, boundaries, log=True)        # Apply physics to objects and boundaries
+running = True      # variable to store state of the loop
 
-# root.after(1, physics(root, objects, boundaries))         # The part that doesnt work :(
-# root.mainloop()
+# Main loop
+while running:
+    # Handles the quit event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
+    # Runs the game physics
+    physics.physics(objects, boundaries)            # Use log=True for debug output |   Use limit=<no. of seconds> to limit the program to a certain amount of time
+    
+# Try to use the limit parameter every time if you intend to use the debug_extract.py
