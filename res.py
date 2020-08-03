8ddd4ffd2d51976b80749515951d04398e357e73
ghost1608 <64543976+ghost-1608@users.noreply.github.com>
2020-08-03 17:42:35 +0530

    Update res.py
    Changed function descriptions to docstrings.

--- res.py
+++ res.py
@@ -3,8 +3,9 @@ import pygame
 
 
 class Ball:
-    # Initial definition
+""" Class used to create a ball object """
     def __init__(self, surface, **kwargs):
+        """ Initial definition """
         self.surface = surface
         self.radius = kwargs.get('radius')
         self.pos_x = kwargs.get('pos_x')
@@ -15,28 +16,32 @@ class Ball:
 
         self.create()
 
-    # Creates the ball
+
     def create(self):
+        """ Creates the ball """
         self.ball = pygame.draw.circle(self.surface, self.colour, (self.pos_x, self.pos_y), self.radius)
 
-    # Moves the ball
+
     def move(self, x, y):
+        """ Moves the ball """
         self.pos_x = x
         self.pos_y = y
         self.surface.fill((255, 255, 255))
         self.create()
         pygame.display.update()
 
-    # Returns the ball's coordinates
+
     def pos(self):
+        """ Returns the ball's coordinates """
         return self.pos_x, self.pos_y, self.pos_x + self.radius * 2, self.pos_y + self.radius * 2
 
     forces = []         # List storing all the forces
 
 
 class Container:
-    # Initial definition
+""" Class used to create a container object """
     def __init__(self, surface, **kwargs):
+        """ Initial definition """
         self.surface = surface
         self.width = kwargs.get('width')
         self.height = kwargs.get('height')
@@ -44,13 +49,12 @@ class Container:
         self.pos_y = kwargs.get('pos_y')
         self.type = 'boundary'
 
-    # Returns the position of the ball
+
     def pos(self):
+        """ Returns the position of the ball """
         x1 = self.pos_x
         x2 = self.pos_x + self.width
         y1 = self.pos_y
         y2 = self.pos_y + self.height
 
         return x1, y1, x2, y2
-
-objects, boundaries = [], []
